8578763786b2a989ea2a7df7b1f0b0c0fba942c6
sanjay <yedagesanjay8@gmail.com>
2026-03-08 23:39:43 +0530

    new

--- sanjay first react project/src/App.jsx
+++ sanjay first react project/src/App.jsx
@@ -1,5 +1,25 @@
+function Child({ calculateSum }) {
+	return (
+		<div>
+			<button onClick={() => calculateSum(10, 20)}>Add</button>
+		</div>
+	);
+}
+
 function App() {
-	return <h1>Hello World</h1>;
+	const handleAddition = (num1, num2) => {
+		const sum = num1 + num2;
+		alert(
+			`First Number: ${num1}\nSecond Number: ${num2}\nAddition: ${sum}`
+		);
+	};
+
+	return (
+		<div>
+			<h2>Addition Using Function Prop</h2>
+			<Child calculateSum={handleAddition} />
+		</div>
+	);
 }
 
 export default App;
