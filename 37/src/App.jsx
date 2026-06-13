b2cdd85e7783abea1fa302d2d1e4a143196a7193
sanjay <yedagesanjay8@gmail.com>
2026-06-13 19:38:06 +0530

    new

--- 37/src/App.jsx
+++ 37/src/App.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import "./App.css";
+import {Bookmark} from "lucide-react";
 
 function App() {
   return (
     <div className="parent">
-        <div className="card">
-            
+      <div className="card">
+        <div className="top">
+          <img className="logo1" src="https://tse3.mm.bing.net/th/id/OIP.YKFZjzosPzcUtBNHe9cAdAHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="Amazon" />
+          <button>Save <Bookmark  /></button>
         </div>
-     
+
+        <div className="center">
+          <h3>Amazon</h3>
+          <spam>3 days ago</spam>
+          <h1>Senior UI/UX Designer</h1>
+          <button>Part-Time</button> <button>Senior Level</button>
+
+        </div>
+        <div className="footer">
+          <div className="line"></div>
+          <spam>$120/hr</spam>
+          <button>Apply Now</button>
+          <p>Mumbai,india</p>
+
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
