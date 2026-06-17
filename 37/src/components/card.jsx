4845cdb0cf8de21c9d61b74413259441841c9486
sanjay <yedagesanjay8@gmail.com>
2026-06-18 01:17:09 +0530

    new

--- 37/src/components/card.jsx
+++ 37/src/components/card.jsx
@@ -1,27 +1,27 @@
 import react from "react";
 import { Bookmark } from "lucide-react";
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className="parent">
             <div className="card">
                 <div className="top">
-                    <img className="logo1" src="https://tse3.mm.bing.net/th/id/OIP.YKFZjzosPzcUtBNHe9cAdAHaEK?r=0&rs=1&pid=ImgDetMain&o=7&rm=3" alt="Amazon" />
+                    <img className="logo1" src={props.brandLogo} alt="logo" />
                     <button>Save <Bookmark size={12} /></button>
                 </div>
 
                 <div className="center">
-                    <h3 ><bold>Amazon <span>5 days ago</span></bold></h3>
+                    <h3 ><bold>{props.companyName} <span>{props.datePosted}</span></bold></h3>
 
-                    <h2>Senior UI/UX Designer</h2>
+                    <h2>{props.post}</h2>
                     <div className="button-02">
-                        <button>Part-Time</button> <button>Senior Level</button>
+                        <button>{props.tag1}</button> <button>{props.tag2}</button>
                     </div>
                 </div>
                 <div className="line">
                 </div>
                 <div className="footer">
-                    <span>$120/hr</span>
+                    <span>{props.pay}</span>
                     <p>Mumbai,india</p>
                     <button>Apply Now</button>
 
