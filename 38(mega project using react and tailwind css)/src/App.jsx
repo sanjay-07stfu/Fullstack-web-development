ff854e7a245594b104c13c15aeede00de333d6cc
sanjay <yedagesanjay8@gmail.com>
2026-06-19 18:49:43 +0530

    new

---
+++ 38(mega project using react and tailwind css)/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="bg-black min-h-screen text-white flex items-center justify-center">
+      <h1 className="text-5xl">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
