40020b3038f23d4c2c77078f16dd04f58b27524f
sanjay <yedagesanjay8@gmail.com>
2026-06-20 11:34:41 +0530

    bjdfjd

---
+++ 38(mega project using react and tailwind css)/39(project )/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-black">
+      <h1 className="bg-black text-white">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
