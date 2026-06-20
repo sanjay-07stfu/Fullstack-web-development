87d4ec0e5cbfbc275e8d1dc2f635d18b74563a4e
sanjay <yedagesanjay8@gmail.com>
2026-06-20 10:58:49 +0530

    newjfief

--- 38(mega project using react and tailwind css)/src/App.jsx
+++ 38(mega project using react and tailwind css)/src/App.jsx
@@ -3,8 +3,10 @@ import "./index.css";
 
 const App = () => {
   return (
-    <div className="bg-black min-h-screen text-white flex items-center justify-center">
-      <h1 className="text-5xl">Hello World</h1>
+    <div className="bg-black min-h-screen h-full w-full text-white flex items-center justify-center">
+      <h1 className="text-5xl" color="white">
+        Hello World
+      </h1>
     </div>
   );
 };
